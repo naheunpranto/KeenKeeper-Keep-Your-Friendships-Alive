91d72ce41fd85face1d39b72f428d11b321e2535
Naheun Alam <naheunpranto258@gmail.com>
2026-06-18 20:18:52 +0600

    footer section is added

---
+++ src/components/FooterSection.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { BiLogoFacebookSquare } from 'react-icons/bi';
+import { FaXTwitter } from 'react-icons/fa6';
+import { RiInstagramFill } from 'react-icons/ri';
+import FooterLogo from '../assets/KeenKeeper.png';
+import { Link } from 'react-router';
+
+const FooterSection = () => {
+  return (
+    <div className="bg-[#244d3f] text-white py-10">
+      <div className="max-w-11/12 md:max-w-10/12 lg:max-w-8/12 mx-auto">
+        <div className="space-y-4">
+          <img
+            src={FooterLogo}
+            className="mx-auto hover:opacity-90 transition duration-300"
+            alt="Footer Logo"
+          />
+
+          <p className="text-center text-gray-300 hover:text-gray-200 transition duration-300">
+            Your personal shelf of meaningful connections. Browse, tend, and
+            nurture the relationships that matter most.
+          </p>
+
+          <div className="text-center">
+            <p className="text-2xl mb-3">Social Links</p>
+
+            <div className="flex gap-4 justify-center items-center mt-2">
+              <Link className="p-3 rounded-full bg-white hover:bg-gray-200 transition duration-300">
+                <RiInstagramFill className="text-black" size={25} />
+              </Link>
+
+              <Link className="p-3 rounded-full bg-white hover:bg-gray-200 transition duration-300">
+                <BiLogoFacebookSquare className="text-black" size={25} />
+              </Link>
+
+              <Link className="p-3 rounded-full bg-white hover:bg-gray-200 transition duration-300">
+                <FaXTwitter className="text-black" size={25} />
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        {/* Divider */}
+        <div className="divider before:bg-gray-500 after:bg-gray-500 my-6"></div>
+
+        <div className="flex flex-col md:flex-row lg:flex-row items-center justify-between text-gray-400">
+          <p className="text-center md:text-left lg:text-left hover:text-gray-200 transition duration-300">
+            © 2026 KeenKeeper. All rights reserved.
+          </p>
+
+          <ul className="flex flex-col md:flex-row lg:flex-row justify-center items-center gap-3">
+            <li className="hover:text-white cursor-pointer transition duration-300">
+              Privacy Policy
+            </li>
+            <li className="hover:text-white cursor-pointer transition duration-300">
+              Terms fo Service
+            </li>
+            <li className="hover:text-white cursor-pointer transition duration-300">
+              Cookies
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FooterSection;
